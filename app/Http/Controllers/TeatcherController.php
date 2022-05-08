736c98f34441a61f4d9b6e899def4debbc101fee
Hamed Al Busaidi <busaidi.nexus@gmail.com>
2022-05-08 09:13:21 +0400

    Initial commit

---
+++ app/Http/Controllers/TeatcherController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Teatcher;
+use Illuminate\Http\Request;
+
+class TeatcherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Teatcher  $teatcher
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Teatcher $teatcher)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Teatcher  $teatcher
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Teatcher $teatcher)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Teatcher  $teatcher
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Teatcher $teatcher)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Teatcher  $teatcher
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Teatcher $teatcher)
+    {
+        //
+    }
+}
