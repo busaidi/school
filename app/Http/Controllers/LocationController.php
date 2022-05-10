25e8f4f4707d2be3ed6d99bf172f78ffa3a54fd5
Hamed Al Busaidi <busaidi.nexus@gmail.com>
2022-05-10 10:52:53 +0400

    setting blade template, befour install loging system auth

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Location;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class LocationController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
@@ -20,7 +21,7 @@ class LocationController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -30,8 +31,8 @@ class LocationController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -41,8 +42,8 @@ class LocationController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Location  $location
-     * @return \Illuminate\Http\Response
+     * @param Location $location
+     * @return Response
      */
     public function show(Location $location)
     {
@@ -52,8 +53,8 @@ class LocationController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Location  $location
-     * @return \Illuminate\Http\Response
+     * @param Location $location
+     * @return Response
      */
     public function edit(Location $location)
     {
@@ -63,9 +64,9 @@ class LocationController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Location  $location
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Location $location
+     * @return Response
      */
     public function update(Request $request, Location $location)
     {
@@ -75,8 +76,8 @@ class LocationController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Location  $location
-     * @return \Illuminate\Http\Response
+     * @param Location $location
+     * @return Response
      */
     public function destroy(Location $location)
     {
--- app/Http/Controllers/TeatcherController.php
+++ app/Http/Controllers/TeatcherController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Teatcher;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TeatcherController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
@@ -20,7 +21,7 @@ class TeatcherController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -30,8 +31,8 @@ class TeatcherController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -41,8 +42,8 @@ class TeatcherController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Teatcher  $teatcher
-     * @return \Illuminate\Http\Response
+     * @param Teatcher $teatcher
+     * @return Response
      */
     public function show(Teatcher $teatcher)
     {
@@ -52,8 +53,8 @@ class TeatcherController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Teatcher  $teatcher
-     * @return \Illuminate\Http\Response
+     * @param Teatcher $teatcher
+     * @return Response
      */
     public function edit(Teatcher $teatcher)
     {
@@ -63,9 +64,9 @@ class TeatcherController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Teatcher  $teatcher
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Teatcher $teatcher
+     * @return Response
      */
     public function update(Request $request, Teatcher $teatcher)
     {
@@ -75,8 +76,8 @@ class TeatcherController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Teatcher  $teatcher
-     * @return \Illuminate\Http\Response
+     * @param Teatcher $teatcher
+     * @return Response
      */
     public function destroy(Teatcher $teatcher)
     {
